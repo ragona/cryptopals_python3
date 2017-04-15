0f6d96e3383f3369db8e4b2a7dd4b00f176536f4
ragona <ryanragona@gmail.com>
2017-04-14 23:33:01 -0700

    added the rest of the instructions

--- s1/c6.py
+++ s1/c6.py
@@ -27,10 +27,33 @@ def decypher():
     f = open("c6.txt", "rb")
     d = f.read()
 
-    # Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
-    # For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE 
-    # worth of bytes, and find the edit distance between them. 
-    # Normalize this result by dividing by KEYSIZE.
+    # 1. Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
+
+    # 2. Write a function to compute the edit distance/Hamming distance between two
+    # strings. The Hamming distance is just the number of differing bits. 
+
+    # 3. For each KEYSIZE, take the first KEYSIZE worth of bytes, 
+    # and the second KEYSIZE  worth of bytes, and find the edit distance 
+    # between them. Normalize this result by dividing by KEYSIZE.
+
+    # 4. The KEYSIZE with the smallest normalized edit distance is probably the key. 
+    # You could proceed perhaps with the smallest 2-3 KEYSIZE values. 
+    # Or take 4 KEYSIZE blocks instead of 2 and average the distances.
+
+    # 5. Now that you probably know the KEYSIZE: 
+    # break the ciphertext into blocks of KEYSIZE length.
+
+    # 6. Now transpose the blocks: make a block that is the 
+    # first byte of every block, and a block that is the 
+    # second byte of every block, and so on.
+
+    # 7. Solve each block as if it was single-character XOR.
+
+    # 8. For each block, the single-byte XOR key that 
+    # produces the best looking histogram is the repeating-key 
+    # XOR key byte for that block. Put them together and 
+    # you have the key.
+
     f.close()
 
 
