c6a1b891ed2fbe61dc168ea6e38c37c06b96dab7
ragona <ryanragona@gmail.com>
2017-04-15 13:53:12 -0700

    progress on c6

--- s1/c6.py
+++ s1/c6.py
@@ -1,5 +1,8 @@
 #Break repeating-key XOR
 
+# Write a function to compute the edit distance/Hamming distance between two
+# strings. The Hamming distance is just the number of differing bits. 
+
 #=====================
 # HAMMING DISTANCE 
 #=====================
@@ -22,24 +25,39 @@ def bit_shift_compare(a, b):
 # DECRYPTION 
 #===============
 
+# For each KEYSIZE, take the first KEYSIZE worth of bytes, 
+# and the second KEYSIZE  worth of bytes, and find the edit distance 
+# between them. Normalize this result by dividing by KEYSIZE.
+
+#ks is keysize, n is number of iterations to perform
+#return diff averaged over n iterations normalized by ks
+def compare_sized_chunks(data, ks, n):
+    diff = 0
+    for i in range(n):
+        x = i * ks
+        y = x + ks
+        a = data[x : y]
+        b = data[y : y + ks]
+        diff += hamming(a, b)
+    return (diff / n) / ks
+
+#===============
+# DECRYPTION 
+#===============
+
 def decypher():
     # Open the file 
     f = open("c6.txt", "rb")
     d = f.read()
 
     # 1. Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
-
-    # 2. Write a function to compute the edit distance/Hamming distance between two
-    # strings. The Hamming distance is just the number of differing bits. 
-
-    # 3. For each KEYSIZE, take the first KEYSIZE worth of bytes, 
-    # and the second KEYSIZE  worth of bytes, and find the edit distance 
-    # between them. Normalize this result by dividing by KEYSIZE.
+    edit_distances = [[ks, compare_sized_chunks(d, ks, 4)] for ks in range(2, 40)]
 
     # 4. The KEYSIZE with the smallest normalized edit distance is probably the key. 
     # You could proceed perhaps with the smallest 2-3 KEYSIZE values. 
     # Or take 4 KEYSIZE blocks instead of 2 and average the distances.
-
+    top_5 = sorted(edit_distances, key=lambda x: x[1])[:5]
+    
     # 5. Now that you probably know the KEYSIZE: 
     # break the ciphertext into blocks of KEYSIZE length.
 
@@ -57,6 +75,5 @@ def decypher():
     f.close()
 
 
-print(hamming(b"this is a test", b"wokka wokka!!!"))
 decypher()
 
