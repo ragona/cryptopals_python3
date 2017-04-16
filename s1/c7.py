8c6ff21d49310be788ad94a88c123d575e15c87b
ragona <ryanragona@gmail.com>
2017-04-16 14:03:40 -0700

    solved c7

---
+++ s1/c7.py
@@ -0,0 +1,22 @@
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64
+
+f = open("c7.txt", "rb")
+d = base64.b64decode(f.read())
+
+cipher = AES.new(b'YELLOW SUBMARINE', AES.MODE_ECB)
+
+print(cipher.decrypt(d))
+
+'''
+AES in ECB mode
+The Base64-encoded content in this file has been encrypted via AES-128 in ECB mode under the key
+
+"YELLOW SUBMARINE".
+(case-sensitive, without the quotes; exactly 16 characters; I like "YELLOW SUBMARINE" because it's exactly 16 bytes long, and now you do too).
+
+Decrypt it. You know the key, after all.
+
+Easiest way: use OpenSSL::Cipher and give it AES-128-ECB as the cipher.
+'''
