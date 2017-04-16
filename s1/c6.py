5cab2459f3d8cc10e7b7139ad37cdc529e52ec7b
ragona <ryanragona@gmail.com>
2017-04-16 09:12:43 -0700

    got it! c6 solved.

--- s1/c6.py
+++ s1/c6.py
@@ -1,7 +1,13 @@
 #Break repeating-key XOR
 
+import base64
+
+#=====================
+# XOR AND SCORRING 
+#=====================
+
 # https://en.wikipedia.org/wiki/Letter_frequency
-freqTable = {"a": 8.16, "b": 1.49, "c": 2.78, "d": 4.25, "e": 12.70, "f": 2.22, "g": 2.01, "h": 6.09, "i": 6.96, "j": 0.15, "k": 0.77, "l": 4.02,
+freqTable = {" ": 13, "a": 8.16, "b": 1.49, "c": 2.78, "d": 4.25, "e": 12.70, "f": 2.22, "g": 2.01, "h": 6.09, "i": 6.96, "j": 0.15, "k": 0.77, "l": 4.02,
              "m": 2.40, "n": 6.74, "o": 7.50, "p": 1.92, "q": 0.09, "r": 5.98, "s": 6.32, "t": 9.05, "u": 2.75, "v": 0.97, "w": 2.36, "x": 0.15, "y": 1.97, "z": 0.07}
 
 def singleCharXor(s, k):
@@ -58,7 +64,7 @@ def compare_sized_chunks(data, ks, n):
 def decypher():
     # Open the file 
     f = open("c6.txt", "rb")
-    d = f.read()
+    d = base64.b64decode(f.read())
 
     # 1. Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
     edit_distances = [[ks, compare_sized_chunks(d, ks, 4)] for ks in range(2, 40)]
@@ -67,7 +73,7 @@ def decypher():
     # You could proceed perhaps with the smallest 2-3 KEYSIZE values. 
     # Or take 4 KEYSIZE blocks instead of 2 and average the distances.
     top_5 = sorted(edit_distances, key=lambda x: x[1])[:5]
-    
+
     # 5. Now that you probably know the KEYSIZE: 
     # break the ciphertext into blocks of KEYSIZE length.
 
@@ -75,27 +81,30 @@ def decypher():
     # first byte of every block, and a block that is the 
     # second byte of every block, and so on.
 
-    #3, 20, 4
-    blocks = [bytearray() for i in range(3)]
+    #2, 5, 29, 3, 18
+    ks = 29 
+    blocks = [bytearray() for i in range(ks)]
     for i, b in enumerate(d):
-        blocks[i%3].append(b)
+        blocks[i%ks].append(b)
 
     # 7. Solve each block as if it was single-character XOR.
     key = []
     for i, block in enumerate(blocks):
-        print("BLOCK {}".format(i))
-        # [print(key, scoreEnglishness(singleCharXor(block, key))) for key in range(30, 40)]
         key_results = [[key, scoreEnglishness(singleCharXor(block, key))] for key in range(1, 255)]
-        key.append(sorted(key_results, key=lambda x: x[1])[-1:][0])
+        sorted_keys = sorted(key_results, key=lambda x: x[1])
+        key.append(sorted_keys[-1][0])
 
-    print(key)
-
-    #WHY IS THE SPACE THE BEST FOR ALL OF THEM :(
+    print("key:'{}'\n".format("".join([chr(k) for k in key])))
         
     # 8. For each block, the single-byte XOR key that 
     # produces the best looking histogram is the repeating-key 
     # XOR key byte for that block. Put them together and 
     # you have the key.
+
+    # rotating decode
+    decoded = "".join([chr(b ^ key[i % len(key)]) for i, b in enumerate(d)])
+
+    print("result:\n{}".format(decoded))
 
     f.close()
 
