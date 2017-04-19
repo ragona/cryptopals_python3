85277cbceb44e0705a455d126c1d02edd9c9c1e4
ragona <ryanragona@gmail.com>
2017-04-18 17:44:57 -0700

    test file

---
+++ test.py
@@ -0,0 +1,6 @@
+#just imports every challenge. since all the
+#challenges just run immediately and print 
+#the reuslt, this file functions as a bootleg
+#series of unittests. used to make sure that 
+#changes in util didn't break stuff.
+import c1, c2, c3, c4, c5, c6, c7, c8, c9
