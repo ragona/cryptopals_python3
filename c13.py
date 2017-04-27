14aed3a4949224f8325d118b5e74e17052d12d16
ragona <ryanragona@gmail.com>
2017-04-27 09:13:43 -0700

    solved c13

--- c13.py
+++ c13.py
@@ -2,6 +2,8 @@ from Crypto import Random
 from pals import utils
 import re
 
+random_key = Random.get_random_bytes(16) 
+
 def kv_parse(s):
     fields = s.split('&')
     pairs = [f.split('=') for f in fields]
@@ -21,17 +23,55 @@ def profile_for(email):
         "role": 'user'
     })
 
+#the way that the application treats the cookies it gets
+def parse_encrypted_profile(b):
+    s = utils.aes_ecb_decrypt(b, random_key).decode('ascii')
+    return kv_parse(s.rstrip('\x04'))
 
-random_key = Random.get_random_bytes(16) 
-
+#the cookie the attacker gets back (we'd b64 this)
 def black_box(user_input):
     return utils.aes_ecb_encrypt(bytes(profile_for(user_input), 'ascii'), random_key)
 
-print(black_box("foo@bar.com"))
+#we need to remove the padding from the encryption
+#which is where the real problem is; we can figure
+#out what the ecb 16 byte result for 'admin' plus
+#padding, then make sure that the first two params
+#('email' and 'uid') are neatly contained within
+#two 16 byte blocks, and then just append the admin
+#block to that 'real' user. this method will then 
+#just strip off the padding at the end, and we have
+#an admin user.
 
+#first we jam in a bunch of 16 byte blocks of 'admin'
+#plus padding to take us to the end of the block
+enc = black_box('A' * 10 + ('admin' + '\x04' * 11) * 10)
+dec = parse_encrypted_profile(enc)
 
+print('===== bad ======')
+print(len(enc), dec)
+blocks = utils.get_blocks(enc, 16)
+[print(len(block), block) for block in blocks]
 
+#grab one of the admin blocks
+tail = blocks[3] 
 
+print('===== good =====')
+enc = black_box('elvtd@foo.com')
+dec = parse_encrypted_profile(enc)
+print(dec)
+blocks = utils.get_blocks(enc, 16)
+[print(len(block), block) for block in blocks]
+
+#grab the first two blocks for the user who will get access
+head = blocks[0] + blocks[1]
+
+#submit the combined blocks as an encrypted cookie
+print('==== combined =====')
+print(parse_encrypted_profile(head + tail))
+
+#email=&uid=10&role=
+#     ^5           ^12
+#email=elvtd@foo.com
 
 '''
 ECB cut-and-paste
