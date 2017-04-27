648ba4324ab91386a3d1698811c8fe6e445f7dec
ragona <ryanragona@gmail.com>
2017-04-27 16:22:23 -0700

    moved ecb byte at a time to utils

--- c12.py
+++ c12.py
@@ -9,49 +9,7 @@ unknown = base64.b64decode(b64unknown)
 def black_box(user_input):
     return utils.aes_ecb_encrypt(user_input + unknown, rand_key)
 
-def solve_unknown():
-    #get blocksize by shoving a bunch of identical
-    #bytes into the function and seeing if we can
-    #detect ECB, along with the blocksize used
-    blocksize = 0
-    for i in range(64):
-        pad = (b'A' * i) * 2
-        if utils.detect_ecb(black_box(pad)):
-            blocksize = i
-            break
-    
-    #we're gonna read the unknown portion by eating 
-    #into it one byte at a time by sending in known
-    #input plus appropriately sized padding such 
-    #that we can randomize the very last byte 
-    solved = b''
-    pad = b'A' * blocksize
-    while True:
-        #pad is the empty bytes at the front
-        pad = b'A' * (blocksize - 1 - (len(solved) % blocksize))
-        #overall size will be multiple of blocksize, including 
-        #the one extra byte that we'll be randomizing
-        size = len(pad) + len(solved) + 1
-        results = {}
-        #send (pad + solved + char) into the box, store 
-        #the first 'size' bytes in our results dict keyed
-        #to the single byte that we used to get the result
-        for j in range(0,255):
-            test = black_box(pad + solved + bytes([j])) 
-            results[test[0:size]] = bytes([j])
-        #send JUST the pad into the black box but test 
-        #the same number of bytes off the front of the
-        #result against the results table and see if 
-        #we have the a matching result in our dict 
-        pad_only = black_box(pad)
-        chunk = pad_only[0:size]
-        if chunk in results:
-            solved += results[chunk]
-        else:
-            break
-    print(solved)
-
-solve_unknown()
+print( utils.ecb_byte_aat(black_box) )
 
 '''
 Byte-at-a-time ECB decryption (Simple)
