7dc0b297c0094fa11a6f8ae9a58c1b645298446e
ragona <ryanragona@gmail.com>
2017-05-02 21:31:05 -0700

    updated test

--- test.py
+++ test.py
@@ -3,4 +3,4 @@
 #the reuslt, this file functions as a bootleg
 #series of unittests. used to make sure that 
 #changes in util didn't break stuff.
-import c01, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13, c14, c15
+import c01, c02, c03, c04, c05, c06, c07, c08, c09, c10, c11, c12, c13, c14, c15, c16
