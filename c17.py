d7df517f9d1aa2a84b503009ecb39bc77fd8d953
ragona <ryanragona@gmail.com>
2017-05-08 16:33:23 -0700

    this one is hard

--- c17.py
+++ c17.py
@@ -5,9 +5,10 @@ import random
 import base64
 
 random_key = 'YELLOW SUBMARINE'#Random.get_random_bytes(16)
-iv = b'0' * 16
+iv = Random.get_random_bytes(16)
 strs = [
-    b'MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=',
+    b'YELLOW SUBMARINE'
+    # b'MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=',
     # b'MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=',
     # b'MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==',
     # b'MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==',
@@ -25,22 +26,29 @@ def black_box():
     aes = AES.new(random_key, AES.MODE_CBC, iv)
     return aes.encrypt(s)
 
-def is_padding_valid(ciphertext):
-    aes = AES.new(random_key, AES.MODE_CBC, iv)
-    s = aes.decrypt(ciphertext)
+def decrypt(ciphertext):
+    return AES.new(random_key, AES.MODE_CBC, iv).decrypt(ciphertext)
+    
+def is_padding_valid(plaintext):
     try:
-        utils.unpad(s)
+        utils.unpad(plaintext)
         return True
     except:
         return False
 
-def decrypt(ciphertext):
-    for i in range(len(ciphertext) // 16):
-        print(i)
+def print_blocks(buffer):
+    blocks = utils.get_blocks(buffer, 16)
+    [print(i, block) for i, block in enumerate(blocks)]
+
+def attack(ciphertext):
+    #wait we're not trying to guess the padding we're trying to force fake good padding
+    #I was thinking about this totally wrong
+    #this also means that we can get multiple "good" results if the "bad" padding happens 
+    #to collide with the real good padding in the last block
     return 'done'
 
 print(
-    decrypt(black_box())
+    attack(black_box())
 )
 '''
 The CBC padding oracle
