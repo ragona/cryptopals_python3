a792ccd61e9a544c985cf195b37300dfc33dbc5d
ragona <ryanragona@gmail.com>
2017-05-11 20:38:32 -0700

    futzing with formatting

--- c19.py
+++ c19.py
@@ -1,8 +1,13 @@
+
+
 '''
 Break fixed-nonce CTR mode using substitutions
-Take your CTR encrypt/decrypt function and fix its nonce value to 0. Generate a random AES key.
+Take your CTR encrypt/decrypt function and fix its nonce value to 0. 
+Generate a random AES key.
 
-In successive encryptions (not in one big running CTR stream), encrypt each line of the base64 decodes of the following, producing multiple independent ciphertexts:
+In successive encryptions (not in one big running CTR stream), encrypt 
+each line of the base64 decodes of the following, producing multiple 
+independent ciphertexts:
 
 SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==
 Q29taW5nIHdpdGggdml2aWQgZmFjZXM=
@@ -46,17 +51,21 @@ VHJhbnNmb3JtZWQgdXR0ZXJseTo=
 QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=
 (This should produce 40 short CTR-encrypted ciphertexts).
 
-Because the CTR nonce wasn't randomized for each encryption, each ciphertext has been encrypted against the same keystream. This is very bad.
+Because the CTR nonce wasn't randomized for each encryption, each ciphertext 
+has been encrypted against the same keystream. This is very bad.
 
-Understanding that, like most stream ciphers (including RC4, and obviously any block cipher run in CTR mode), the actual "encryption" of a byte of data boils down to a single XOR operation, it should be plain that:
+Understanding that, like most stream ciphers (including RC4, and obviously 
+any block cipher run in CTR mode), the actual "encryption" of a byte of data 
+boils down to a single XOR operation, it should be plain that:
 
 CIPHERTEXT-BYTE XOR PLAINTEXT-BYTE = KEYSTREAM-BYTE
 And since the keystream is the same for every ciphertext:
 
-CIPHERTEXT-BYTE XOR KEYSTREAM-BYTE = PLAINTEXT-BYTE (ie, "you don't
-say!")
-Attack this cryptosystem piecemeal: guess letters, use expected English language frequence to validate guesses, catch common English trigrams, and so on.
+CIPHERTEXT-BYTE XOR KEYSTREAM-BYTE = PLAINTEXT-BYTE (ie, "you don't say!")
+Attack this cryptosystem piecemeal: guess letters, use expected English language 
+frequence to validate guesses, catch common English trigrams, and so on.
 
 Don't overthink it.
-Points for automating this, but part of the reason I'm having you do this is that I think this approach is suboptimal.
+Points for automating this, but part of the reason I'm having you do this is 
+that I think this approach is suboptimal.
 '''
