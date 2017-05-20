b8701d868af2964e78ef70545e099adc971c3bfb
ragona <ryanragona@gmail.com>
2017-05-20 13:50:38 -0700

    updated test

--- test.py
+++ test.py
@@ -3,6 +3,10 @@
 #the reuslt, this file functions as a bootleg
 #series of unittests. used to make sure that 
 #changes in util didn't break stuff.
+
+#set 1
 import c01, c02, c03, c04, c05, c06, c07, c08, c09
+#set 2
 import c10, c11, c12, c13, c14, c15, c16
-import c17, c18, c19, c20, c21, c22
+#set 3
+import c17, c18, c19, c20, c21, c22, c23 #skipping c24, takes a while to run
