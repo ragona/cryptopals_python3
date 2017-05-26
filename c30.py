fc83805378d335fcc3eeae840767ec783b4207ff
ragona <ryanragona@gmail.com>
2017-05-25 23:36:17 -0700

    solved c30

--- c30.py
+++ c30.py
@@ -8,7 +8,7 @@ import struct
 #============
 
 def mac(data):
-    return MD4(b'banana' + data).hexdigest()
+    return MD4(b'foobanana' + data).hexdigest()
 
 def validate(msg, foo):
     return mac(msg) == foo
@@ -20,10 +20,8 @@ def validate(msg, foo):
 def md4_ext(msg, good_mac, inject, key_len):
     #the 'unwound' state of the md4 algorithm
     state = struct.unpack('<4I', unhexlify(good_mac))
-    # print('state')
-    print("UW STATE", state[0], state[1], state[2], state[3])
     #pad with the length of the key (will need to automate) 
-    forged_message = utils.md_pad((b'A' * key_len) + msg)[key_len:] + inject
+    forged_message = utils.md4_pad(b'A' * key_len + msg)[key_len:] + inject
     #make new mac
     forged_mac = MD4(inject, (key_len + len(forged_message)), state[0], state[1], state[2], state[3]).hexdigest()
     return forged_message, forged_mac
@@ -33,17 +31,14 @@ msg = b'comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20
 inject = b';admin=true'
 good_mac = mac(msg)
 
-bad_msg, bad_mac = md4_ext(msg, good_mac, inject, 6)
-
-# print(good_mac)
 # our attack; guess at initial key prefix sizes
-# for i in range(100):
-#     #generate forged mac + msg
-#     bad_msg, bad_mac = md4_ext(msg, good_mac, inject, i)
-#     #submit to server, see if it accepts it 
-#     if validate(bad_msg, bad_mac):
-#         print('forged message, key len is', i)
-#         break
+for i in range(100):
+    #generate forged mac + msg
+    bad_msg, bad_mac = md4_ext(msg, good_mac, inject, i)
+    #submit to server, see if it accepts it 
+    if validate(bad_msg, bad_mac):
+        print('forged message, key len is', i)
+        break
 
 
 '''
