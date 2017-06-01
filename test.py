cb7578a4cfb1c60e65fe0082a5804af7375adcc7
ragona <ryanragona@gmail.com>
2017-05-31 18:57:36 -0700

    updated test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #just imports every challenge. since all the
 #challenges just run immediately and print 
-#the reuslt, this file functions as a bootleg
+#the result, this file functions as a bootleg
 #series of unittests. used to make sure that 
 #changes in util didn't break stuff.
 
@@ -10,3 +10,7 @@ import c01, c02, c03, c04, c05, c06, c07, c08, c09
 import c10, c11, c12, c13, c14, c15, c16
 #set 3
 import c17, c18, c19, c20, c21, c22, c23 #skipping c24, takes a while to run
+#set 4 
+import c25, c26, c27, c28, c29, c30, c32 #skipping c31, server
+#set 5
+import c33, c34, c35, c36
