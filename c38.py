21d721b1f6b1ba0057e0a1bd55a485f5ec8cc520
ragona <ryanragona@gmail.com>
2017-06-01 21:32:26 -0700

    c38 setup

--- c38.py
+++ c38.py
@@ -1,3 +1,72 @@
+'''
+Going procedural for this one for brevity and clarity. 
+For the more reasonably architected examples see c36/37.
+
+To brute force recover a password in a reasonable amount of time
+you need the client hmac and the salt. From there you can start 
+all the way back at generating the server's x and v values, where
+x is H(salt, dictionary_password_guess). I initially spent a while
+staring at this and seeing if I could send interesting B, u, salt 
+variables and unwind the algorithm somehow, but I think the approach
+with the best big O time is to just brute force it starting from
+the beginning until you manage to find something that matches the 
+client hmac. Note that this only relates to a MITM situation; if 
+you had a leaked password database, you'd only have to do the first
+calculation of x and v, and if you got a v that matched the stored 
+verifier you'd have the password, and this would be a bit faster
+than the MITM version. 
+'''
+
+import hashlib
+from pals.srp import H, hash, hmac_sha256
+from Crypto.Random import random
+
+#constants
+g = 2
+k = 3
+n = int(('ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024'
+         'e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd'
+         '3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec'
+         '6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f'
+         '24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361'
+         'c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552'
+         'bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff'
+         'fffffffffffff'), 16) #just for formatting
+
+#helper
+rand = lambda size: random.randint(0, 2<<size)
+
+#shared info
+password = 'banananutbread'
+
+#server 
+salt = rand(32)
+x = H(salt, password)
+v = pow(g, x, n)
+
+#client to server
+a = rand(32)
+A = pow(g, a, n)
+
+#server to client
+b = rand(32)
+B = pow(g, b, n)
+u = rand(128)
+
+#client
+x = H(salt, password)
+S = pow(B, (a + u * x), n) 
+cK = hash(str(S))
+cHK = hmac_sha256(salt, cK)
+
+#server
+S = pow((A * pow(v, u, n)), b, n)
+sK = hash(str(S))
+sHK = hmac_sha256(salt, sK)
+
+#verify
+print(sHK == cHK)
+
 
 '''
 Offline dictionary attack on simplified SRP
@@ -19,11 +88,14 @@ C->S
 Send HMAC-SHA256(K, salt)
 S->C
 Send "OK" if HMAC-SHA256(K, salt) validates
-Note that in this protocol, the server's "B" parameter doesn't depend on the password (it's just a Diffie Hellman public key).
+Note that in this protocol, the server's "B" 
+parameter doesn't depend on the password (it's 
+just a Diffie Hellman public key).
 
 Make sure the protocol works given a valid password.
 
-Now, run the protocol as a MITM attacker: pose as the server and use arbitrary values for b, B, u, and salt.
+Now, run the protocol as a MITM attacker: pose as the 
+server and use arbitrary values for b, B, u, and salt.
 
 Crack the password from A's HMAC-SHA256(K, salt).
 '''
