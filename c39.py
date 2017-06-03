5e5b132f34c6760db926e9354efc8f9a6123a338
ragona <ryanragona@gmail.com>
2017-06-03 13:42:33 -0700

    added prime number gen

--- c39.py
+++ c39.py
@@ -1,4 +1,24 @@
+import itertools
 
+#a**m=1(%n)
+def get_primes_erat(n):
+  return list(itertools.takewhile(lambda p: p<n, erat2()))
+
+def erat2( ):
+    D = {  }
+    yield 2
+    for q in itertools.islice(itertools.count(3), 0, None, 2):
+        p = D.pop(q, None)
+        if p is None:
+            D[q*q] = q
+            yield q
+        else:
+            x = p + q
+            while x in D or not (x&1):
+                x += p
+            D[x] = p
+
+print(get_primes_erat(1000000000))
 '''
 Implement RSA
 There are two annoying things about implementing RSA. Both of 
