ffd3903a07ff0ac4a34e940ced1af75eb8fc7ac7
ragona <ryanragona@gmail.com>
2017-06-04 09:57:32 -0700

    oh, crypto has primegen

--- c39.py
+++ c39.py
@@ -1,24 +1,9 @@
-import itertools
+from Crypto.Util import number
+from os import urandom
 
-#a**m=1(%n)
-def get_primes_erat(n):
-  return list(itertools.takewhile(lambda p: p<n, erat2()))
+p = number.getPrime(1024, urandom)
 
-def erat2( ):
-    D = {  }
-    yield 2
-    for q in itertools.islice(itertools.count(3), 0, None, 2):
-        p = D.pop(q, None)
-        if p is None:
-            D[q*q] = q
-            yield q
-        else:
-            x = p + q
-            while x in D or not (x&1):
-                x += p
-            D[x] = p
-
-print(get_primes_erat(1000000000))
+print(p)
 '''
 Implement RSA
 There are two annoying things about implementing RSA. Both of 
