bb4585e414327c31cc3715194a4e9949c699445b
ragona <ryanragona@gmail.com>
2017-06-05 21:35:44 -0700

    setup

--- c40.py
+++ c40.py
@@ -1,3 +1,32 @@
+from Crypto.Util.number import inverse
+from pals.RSA import RSA
+
+msg = b'some secret message'
+
+pub_0 = RSA.generate_keys(1024, 3)[0]
+pub_1 = RSA.generate_keys(1024, 3)[0]
+pub_2 = RSA.generate_keys(1024, 3)[0]
+
+c_0 = RSA.encrypt(msg, pub_0)
+c_1 = RSA.encrypt(msg, pub_1)
+c_2 = RSA.encrypt(msg, pub_2)
+
+n_0 = pub_0[1]
+n_1 = pub_1[1]
+n_2 = pub_2[1]
+
+m_s_0 = n_1 * n_2
+m_s_1 = n_0 * n_2
+m_s_2 = n_0 * n_1
+
+N_012 = n_0 * n_1 * n_2
+
+result = ((c_0 * m_s_0 * inverse(m_s_0, n_0)) +
+          (c_1 * m_s_1 * inverse(m_s_1, n_1)) +
+          (c_2 * m_s_2 * inverse(m_s_2, n_2)) % N_012)
+
+
+#do cube root 
 
 '''
 Implement an E=3 RSA Broadcast attack
