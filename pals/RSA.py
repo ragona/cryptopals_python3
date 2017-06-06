eee431250338292188e25a3d1c60b5e12acb4a9f
ragona <ryanragona@gmail.com>
2017-06-05 20:32:21 -0700

    added retry for when inverse doesn't work properly

--- pals/RSA.py
+++ pals/RSA.py
@@ -10,13 +10,18 @@ def bytes_from_int(i):
 
 class RSA:
 
-    def generate_keys(key_size):
-        p = number.getPrime(key_size // 2, urandom)
-        q = number.getPrime(key_size // 2, urandom)
-        n = p * q
-        et = (p-1)*(q-1)
-        e = 65537 #TODO: should this be random? need to research.
-        d = number.inverse(e, et)
+    def generate_keys(key_size, e=65537):
+        #d of 1 indicates that the number didn't inverse 
+        #appropriately, so we try again until it works.
+        #this doesn't happen (often?) when e is 65537, but
+        #does happen often when e is 3. 
+        d = 1
+        while d == 1:
+            p = number.getPrime(key_size // 2, urandom)
+            q = number.getPrime(key_size // 2, urandom)
+            n = p * q
+            et = (p-1)*(q-1)
+            d = number.inverse(e, et)
         return (e, n), (d, n) #public, private
 
     def encrypt(m, key):
@@ -25,3 +30,12 @@ class RSA:
     def decrypt(m, key):
         c = pow(m, key[0], key[1])#d, n)
         return bytes_from_int(c)
+
+    def _d():
+        p = number.getPrime(key_size // 2, urandom)
+        q = number.getPrime(key_size // 2, urandom)
+        n = p * q
+        et = (p-1)*(q-1)
+        e = 3#65537 #TODO: should this be random? need to research.
+        d = number.inverse(e, et)
+        return d
