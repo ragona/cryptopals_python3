8be1de91ff0c765bec60a25eca2a751ec9105d28
ragona <ryanragona@gmail.com>
2017-06-08 19:33:04 -0700

    general setup

--- c41.py
+++ c41.py
@@ -1,17 +1,63 @@
+from Crypto.Util.number import inverse
+from pals.RSA import RSA, bytes_from_int
+import json, time, hashlib
+
+public, private = RSA.generate_keys()
+seen = set()
+
+def generate_blob(ssn):
+    blob = {
+        'time': int(time.time()),
+        'ssn': ssn
+    }
+    return json.dumps(blob) 
+
+def encrypt_blob(blob):
+    return RSA.encrypt(blob.encode('utf-8'), public)
+
+def decypher_blob(ciphertext):
+    msg = RSA.decrypt(ciphertext, private)
+    hsh = hashlib.sha1(msg).digest()
+    if hsh in seen:
+        return 'nope'
+    seen.add(hsh)
+    return msg
+
+blob = generate_blob('555-55-5555')
+c = encrypt_blob(blob)
+E = public[0]
+N = public[1] 
+S = 10 % N
+
+
+print(decypher_blob(c))
+print(decypher_blob(c))
+print(decypher_blob(c))
 
 '''
 Implement unpadded message recovery oracle
-Nate Lawson says we should stop calling it "RSA padding" and start calling it "RSA armoring". Here's why.
+Nate Lawson says we should stop calling it "RSA padding" a
+nd start calling it "RSA armoring". Here's why.
 
-Imagine a web application, again with the Javascript encryption, taking RSA-encrypted messages which (again: Javascript) aren't padded before encryption at all.
+Imagine a web application, again with the Javascript 
+encryption, taking RSA-encrypted messages which (again: 
+Javascript) aren't padded before encryption at all.
 
-You can submit an arbitrary RSA blob and the server will return plaintext. But you can't submit the same message twice: let's say the server keeps hashes of previous messages for some liveness interval, and that the message has an embedded timestamp:
+You can submit an arbitrary RSA blob and the server will 
+return plaintext. But you can't submit the same message 
+twice: let's say the server keeps hashes of previous messages 
+for some liveness interval, and that the message has an 
+embedded timestamp:
 
 {
   time: 1356304276,
   social: '555-55-5555',
 }
-You'd like to capture other people's messages and use the server to decrypt them. But when you try, the server takes the hash of the ciphertext and uses it to reject the request. Any bit you flip in the ciphertext irrevocably scrambles the decryption.
+You'd like to capture other people's messages and use the 
+server to decrypt them. But when you try, the server takes 
+the hash of the ciphertext and uses it to reject the request. 
+Any bit you flip in the ciphertext irrevocably scrambles the 
+decryption.
 
 This turns out to be trivially breakable:
 
@@ -20,7 +66,8 @@ Let N and E be the public modulus and exponent respectively
 Let S be a random number > 1 mod N. Doesn't matter what.
 Now:
 C' = ((S**E mod N) C) mod N
-Submit C', which appears totally different from C, to the server, recovering P', which appears totally different from P
+Submit C', which appears totally different from C, to the server, 
+recovering P', which appears totally different from P
 Now:
           P'
     P = -----  mod N
@@ -30,5 +77,6 @@ Oops!
 Implement that attack.
 
 Careful about division in cyclic groups.
-Remember: you don't simply divide mod N; you multiply by the multiplicative inverse mod N. So you'll need a modinv() function.
+Remember: you don't simply divide mod N; you multiply by the 
+multiplicative inverse mod N. So you'll need a modinv() function.
 '''
