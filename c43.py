3e0d0bdadbc0d1d43cb740115855be7a8a55af4c
ragona <ryanragona@gmail.com>
2017-06-14 16:48:10 -0700

    implemented sign

--- c43.py
+++ c43.py
@@ -1,3 +1,77 @@
+from hashlib import sha1
+from Crypto.Random import random
+from Crypto.Util.number import inverse
+
+p = int(('800000000000000089e1855218a0e7dac38136ffafa72eda7'
+         '859f2171e25e65eac698c1702578b07dc2a1076da241c76c6'
+         '2d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebe'
+         'ac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2'
+         'b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc87'
+         '1a584471bb1'), 16)
+
+q = int('f4f47f05794b256174bba6e9b396a7707e563c5b', 16)
+
+g = int(('5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119'
+         '458fef538b8fa4046c8db53039db620c094c9fa077ef389b5'
+         '322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a047'
+         '0f5b64c36b625a097f1651fe775323556fe00b3608c887892'
+         '878480e99041be601a62166ca6894bdd41a7054ec89f756ba'
+         '9fc95302291') , 16)
+
+def per_user_keys():
+  x = random.randint(1, 2<<64) #how big should this number be? 
+  y = pow(g, x, p)
+  return (y, x) #public / private -- should be packaged with p,q,g)
+
+def sign(message, x):
+  h = int.from_bytes(sha1(message).digest(), 'big')
+  r = 0
+  #if r is 0 we need to generate a new k
+  while r == 0:
+    k = random.randint(0, 2<<32)
+    r = pow(g, k, p) % q
+  i = inverse(k, q)
+  s = i*(h+r*x) % q
+  if s == 0:
+    #we need to try again with a different k in this case
+    return sign(message)
+  return (r, s)
+
+pub, pri = per_user_keys()
+print(sign(b'foo', pri))
+
+
+
+
+'''
+The first part of the DSA algorithm is the public key and private key generation, which can be described as:
+
+Choose a prime number q, which is called the prime divisor.
+Choose another primer number p, such that p-1 mod q = 0. p is called the prime modulus.
+Choose an integer g, such that 1 < g < p, g**q mod p = 1 and g = h**((p–1)/q) mod p. q is also called g's multiplicative order modulo p.
+Choose an integer, such that 0 < x < q.
+Compute y as g**x mod p.
+Package the public key as {p,q,g,y}.
+Package the private key as {p,q,g,x}.
+The second part of the DSA algorithm is the signature generation and signature verification, which can be described as:
+
+To generate a message signature, the sender can follow these steps:
+
+Generate the message digest h, using a hash algorithm like SHA1.
+Generate a random number k, such that 0 < k < q.
+Compute r as (g**k mod p) mod q. If r = 0, select a different k.
+Compute i, such that k*i mod q = 1. i is called the modular multiplicative inverse of k modulo q.
+Compute s = i*(h+r*x) mod q. If s = 0, select a different k.
+Package the digital signature as {r,s}.
+To verify a message signature, the receiver of the message and the digital signature can follow these steps:
+
+Generate the message digest h, using the same hash algorithm.
+Compute w, such that s*w mod q = 1. w is called the modular multiplicative inverse of s modulo q.
+Compute u1 = h*w mod q.
+Compute u2 = r*w mod q.
+Compute v = (((g**u1)*(y**u2)) mod p) mod q.
+If v == r, the digital signature is valid.
+'''
 
 '''
 DSA key recovery from nonce
