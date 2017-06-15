47314ed2573fb173e939971ddbd429fd2be7233c
ragona <ryanragona@gmail.com>
2017-06-14 18:00:12 -0700

    moved dsa to module

--- c43.py
+++ c43.py
@@ -1,57 +1,8 @@
-from hashlib import sha1
-from Crypto.Random import random
-from Crypto.Util.number import inverse
+from pals.DSA import DSA, H
 
-p = int(('800000000000000089e1855218a0e7dac38136ffafa72eda7'
-         '859f2171e25e65eac698c1702578b07dc2a1076da241c76c6'
-         '2d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebe'
-         'ac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2'
-         'b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc87'
-         '1a584471bb1'), 16)
-
-q = int('f4f47f05794b256174bba6e9b396a7707e563c5b', 16)
-
-g = int(('5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119'
-         '458fef538b8fa4046c8db53039db620c094c9fa077ef389b5'
-         '322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a047'
-         '0f5b64c36b625a097f1651fe775323556fe00b3608c887892'
-         '878480e99041be601a62166ca6894bdd41a7054ec89f756ba'
-         '9fc95302291') , 16)
-
-def H(message):
-  return int.from_bytes(sha1(message).digest(), 'big') 
-
-def per_user_keys():
-  x = random.randint(1, 2<<64) #how big should this number be? 
-  y = pow(g, x, p)
-  return (y, x) #public / private -- should be packaged with p,q,g)
-
-def sign(message, x):
-  h = H(message)
-  r = 0
-  #if r is 0 we need to generate a new k
-  while r == 0:
-    k = random.randint(0, 2<<32)
-    r = pow(g, k, p) % q
-  i = inverse(k, q)
-  s = i*(h+r*x) % q
-  if s == 0:
-    #we need to try again with a different k in this case
-    return sign(message)
-  return (r, s)
-
-def verify(message, signature, y):
-  r, s = signature
-  h = H(message)
-  w = inverse(s, q)
-  u1 = h * w % q 
-  u2 = r * w % q
-  v = (pow(g, u1, p) * pow(y, u2, p) % p) % q
-  return v == r
-
-pub, pri = per_user_keys()
-signature = sign(b'foo', pri)
-valid = verify(b'foo', signature, pub)
+pub, pri = DSA.generate_user_key_pair()
+signature = DSA.sign(b'foo', pri)
+valid = DSA.verify(b'foo', signature, pub)
 
 print(valid)
 
