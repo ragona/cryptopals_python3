1e791431c6571fe29389d06be9c2302dbab1d254
ragona <ryanragona@gmail.com>
2017-06-24 13:32:28 -0700

    added signature verification that isn't vulnerable to bad padding

--- pals/RSA.py
+++ pals/RSA.py
@@ -38,14 +38,14 @@ Version 1.5
    encryption block EB equal to k.
 
 '''
-#01
+
 def pkcs115_hash_pad(M, n, block_type=1):
         D = sha1(M).digest()
         k = n.bit_length() // 8
         if block_type == 1:
             BT = b'\x01' 
             PS = (k - 3 - len(D)) * b'\xFF' 
-        elif block_type = 2:
+        elif block_type == 2:
             BT = b'\x02' 
             PS = Random.new().read(k - 3 - len(D)) 
         return b'\x00' + BT + PS + b'\x00' + D 
@@ -69,21 +69,47 @@ class RSA:
             d = number.inverse(e, et)
         return (e, n), (d, n) #public, private
 
-    def encrypt(m, key):
-        return pow(bytes_to_int(m), key[0], key[1]) #e, n
+    def encrypt(m, public_key):
+        return pow(bytes_to_int(m), public_key[0], public_key[1]) #e, n
 
-    def decrypt(m, key):
-        c = pow(m, key[0], key[1]) #d, n
+    def decrypt(m, private_key):
+        c = pow(m, private_key[0], private_key[1]) #d, n
         return int_to_bytes(c)
 
+    '''
+    This is the opposite of a normal operation, we 'decrypt' to 
+    generate 'plaintext' bytes that will be later verified using 
+    a public key.
+    '''
     def generate_signature(message, private_key):
         block = pkcs115_hash_pad(message, private_key[1])
         return RSA.decrypt(bytes_to_int(block), private_key)    
 
-    def verify_signature(signature, message, public_key):
+    '''
+    Bad implementation of signature verification; it looks for the
+    right general pattern, but doesn't check all of the padding. 
+    '''
+    def verify_signature_vulnerable(signature, message, public_key):
         s = b'\x00' + int_to_bytes(RSA.encrypt(signature, public_key))
         r = re.compile(b'\x00\x01\xFF+?\x00(.{20})', re.DOTALL) #Vulnerable implementation
         g = re.match(r, s)
         if g is None:
             return False
         return g[1] == sha1(message).digest() 
+
+    '''
+    Fixes the above vulnerability by enforcing that the pkcs padding 
+    exactly matches, instead of just looking for the general pattern
+    and testing to see if the sha1 of the message is matched. 
+    '''
+    def verify_signature(signature, message, public_key, block_type=1):
+        #reverse the 'decrypt' done by generate_signature
+        plain = b'\x00' + int_to_bytes(RSA.encrypt(signature, public_key))
+        #pad user submitted message
+        test = pkcs115_hash_pad(message, public_key[1], block_type)
+        #verify 
+        if plain != test:
+            return False
+        return True
+
+
