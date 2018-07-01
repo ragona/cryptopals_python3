82cec7238a1ac9c148c2d87e0e9e6c9bba4968ec
ragona <ryanragona@gmail.com>
2018-07-01 11:21:06 -0700

    cleaning up old challenges

--- c01.py
+++ c01.py
@@ -1,4 +1,27 @@
-'''
+from binascii import unhexlify
+from base64 import b64encode
+
+
+def hex_to_b64(s):
+    """
+    Converts hex encoded string to base 64 encoded string
+    :param s: Hex encoded string
+    :return:  Base64 encoded string
+    """
+    return b64encode(unhexlify(s))
+
+
+def main():
+    a = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    b = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+    assert hex_to_b64(a) == b
+
+
+if __name__ == '__main__':
+    main()
+
+"""
 Convert hex to base64
 The string:
 
@@ -8,11 +31,4 @@ Should produce:
 SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 So go ahead and make that happen. 
 You'll need to use this code for the rest of the exercises.
-'''
-
-import binascii
-import base64
-
-s = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-
-print(base64.b64encode(binascii.unhexlify(s)))
+"""
--- c02.py
+++ c02.py
@@ -1,4 +1,29 @@
-'''
+import binascii
+
+
+def fixed_xor(a, b):
+    """
+    Takes two equal-length buffers and produces their byte-by-byte XOR combination
+    """
+    if len(a) != len(b):
+        raise ValueError(f"Expected a and b to be the same length; got {len(a)} vs {len(b)}")
+
+    return bytes([a[i] ^ b[i] for i in range(len(a))])
+
+
+def main():
+    a = binascii.unhexlify(b'1c0111001f010100061a024b53535009181c')
+    b = binascii.unhexlify(b'686974207468652062756c6c277320657965')
+    x = binascii.hexlify(fixed_xor(a, b))
+
+    assert x == b'746865206b696420646f6e277420706c6179'
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
 Fixed XOR
 Write a function that takes two equal-length buffers and produces their XOR combination.
 
@@ -11,14 +36,4 @@ If your function works properly, then when you feed it the string:
 ... should produce:
 
 746865206b696420646f6e277420706c6179
-'''
-
-import binascii
-
-a = binascii.unhexlify(b'1c0111001f010100061a024b53535009181c')
-b = binascii.unhexlify(b'686974207468652062756c6c277320657965')
-
-def fixedXor(a, b):
-    return bytes([a[i] ^ b[i] for i in range(len(a))])
-
-print(binascii.hexlify(fixedXor(a, b)))
+"""
