f5d3ec29c4fec43af5250c6260ee46ac2c42b82a
ragona <ryanragona@gmail.com>
2018-07-04 14:16:14 -0700

    cleaned up c04

--- c04.py
+++ c04.py
@@ -1,28 +1,34 @@
-'''
+from binascii import unhexlify
+from pals.freq_analysis import most_english_definition
+
+
+def main():
+    # open the challenge file
+    with open("files/c4.txt", "rb") as f:
+        most_english = ""
+        best_score = 0
+        for line in f.readlines():
+            # try each single character (0-255) key on this line, get the result that
+            # best fits the english frequency scoring table.
+            line_result = most_english_definition(
+                unhexlify(line.strip())
+            )
+            # if this is the highest score, store it
+            if line_result[0] > best_score:
+                best_score = line_result[0]
+                most_english = line_result[1]
+
+        # output our best result
+        print(most_english)
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
 Detect single-character XOR
 One of the 60-character strings in this file has been encrypted by single-character XOR.
 
 Find it.
-'''
-
-# load the file
-# for each line in the file, xor the unhexed string against chars between 0-255
-# evaluate the result for "englishness" by scoring with english letter frequency 
-# return the "most english" decryption for that line
-# if that decryption is more english than the best thing we've seen, declare it the frontrunner
-# print the best decryption we've seen
-
-from binascii import unhexlify
-from pals.freq_analysis import most_english_definition
-
-with open("files/c4.txt", "rb") as f:
-    data = f.read()
-    lines = data.split(b'\n')
-    most_english = ""
-    best_score = 0
-    for line in lines:
-        line_result = most_english_definition(unhexlify(line))
-        if line_result[0] > best_score:
-            best_score = line_result[0]
-            most_english = line_result[1]
-    print(most_english)
+"""
