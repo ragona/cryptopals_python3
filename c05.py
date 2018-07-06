40f0d1eff38a318092204b2dc7f9dda6240b393a
ragona <ryanragona@gmail.com>
2018-07-06 11:35:38 -0700

    minor formatting changes

--- c05.py
+++ c05.py
@@ -1,10 +1,5 @@
 from binascii import hexlify
 
-k = b"ICE"
-phrase = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-want = b"0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272\
-a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
-
 
 def encrypt(message, key):
     """
@@ -17,6 +12,11 @@ def encrypt(message, key):
 
 
 def main():
+    # constants
+    k = b"ICE"
+    phrase = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    want = b"0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
     # encrypt with rotating key
     ciphertext = encrypt(phrase, k)
 
