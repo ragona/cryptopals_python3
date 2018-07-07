9c1756b52405a0e87edfec60023ca5fae23712d2
ragona <ryanragona@gmail.com>
2018-07-07 10:19:14 -0700

    cleaned up, and actually implemented cbc decrypt

--- c10.py
+++ c10.py
@@ -1,26 +1,70 @@
-import binascii
-import base64
+from base64 import b64decode
 from Crypto.Cipher import AES
-from Crypto import Random
-from pals import utils
-
-a = utils.pad(b"YELLOW SUBMARINE", 16)
-b = utils.aes_ecb_encrypt(a, b'YELLOW SUBMARINE')
-c = utils.aes_ecb_decrypt(b, b'YELLOW SUBMARINE')
-
-block_size = 16
-key = b'YELLOW SUBMARINE'
-iv = b'0' * block_size
-
-with open('files/c10.txt', 'rb') as f:
-    data = base64.b64decode(f.read())
-    dec = utils.aes_cbc_decrypt(data, key, iv)
-    enc = utils.aes_cbc_encrypt(dec, key, iv)
-    print(dec) #make sure the decrypt is intelligible 
-    print("encrypt matches file? {}".format(enc == data))
+from pals.utils import pad, unpad
 
 
+def aes_cbc_encrypt(data, key, iv):
+    # make data fit within 16 byte blocks
+    data = pad(data, 16)
+    # prepare cipher
+    cipher = AES.new(key, AES.MODE_ECB)
+    # resulting ciphertext buffer that we'll return
+    ciphertext = bytearray()
+    # first block is the iv
+    block = iv
+    # do the encryption
+    for i in range(0, len(data), 16):
+        # get the plaintext block
+        plaintext = data[i: i + 16]
+        # xor it with the previous block
+        xord_block = bytes([plaintext[i] ^ block[i] for i in range(16)])
+        # store that ciphertext block for use next iteration
+        block = cipher.encrypt(xord_block)
+        # append to overall ciphertext
+        ciphertext += block
+    # return immutable bytes object rather than bytearray
+    return bytes(ciphertext)
 
+
+def aes_cbc_decrypt(data, key, iv):
+    cipher = AES.new(key, AES.MODE_ECB)
+    # resulting plaintext buffer
+    plaintext = bytearray()
+    # the block we'll xor with the decrypted (this will be either the iv or some ciphertext)
+    xor_block = iv
+    # do the decryption
+    for i in range(0, len(data), 16):
+        # get the ciphertext block
+        ciphertext = data[i: i + 16]
+        # decrypt it; this gets us the block that has been xor'd
+        decrypted = cipher.decrypt(ciphertext)
+        # undo the xor to get the plaintext
+        pt_block = bytes([xor_block[i] ^ decrypted[i] for i in range(16)])
+        # store that ciphertext; we'll use it next round
+        xor_block = ciphertext
+        # append to overall plaintext
+        plaintext += pt_block
+    # return immutable bytes object rather than bytearray (and unpad)
+    return bytes(unpad(plaintext))
+
+
+def main():
+    key = b'YELLOW SUBMARINE'
+    iv = b'0' * 16
+
+    with open('files/c10.txt', 'rb') as f:
+        # read the file and get the raw bytes
+        data = b64decode(f.read())
+        # decrypt it
+        dec = aes_cbc_decrypt(data, key, iv)
+        # re-encrypt it
+        enc = aes_cbc_encrypt(dec, key, iv)
+        # verify our implementation works by asserting that we should back to the original bytes
+        assert enc == data
+
+
+if __name__ == '__main__':
+    main()
 
 '''
 Implement CBC mode
