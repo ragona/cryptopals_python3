5b6ee8810ef585af47cef5b64b1c4f44517b9246
ragona <ryanragona@gmail.com>
2018-07-25 09:01:31 -0700

    moved updated cbc definitions into ciphers

--- pals/ciphers.py
+++ pals/ciphers.py
@@ -1,6 +1,5 @@
 from Crypto.Cipher import AES
-from pals.utils import pad
-from pals.utils import xor
+from pals.utils import pad, unpad, xor
 
 
 def aes_ecb_encrypt(data, key):
@@ -14,17 +13,29 @@ def aes_ecb_decrypt(data, key):
 
 
 def aes_cbc_encrypt(data, key, iv):
-    cipher = AES.new(key, AES.MODE_ECB)
-    block_size = len(iv)
-    results = bytes()
-    ciphertext = iv
     data = pad(data, 16)
-    for i in range(0, len(data), block_size):
-        plaintext = data[i: i + block_size]
-        xord = bytes([plaintext[i] ^ ciphertext[i] for i in range(block_size)])
-        ciphertext = cipher.encrypt(xord)
-        results += ciphertext
-    return results
+    cipher = AES.new(key, AES.MODE_ECB)
+    ciphertext = bytearray()
+    block = iv
+    for i in range(0, len(data), 16):
+        plaintext = data[i: i + 16]
+        xord_block = bytes([plaintext[i] ^ block[i] for i in range(16)])
+        block = cipher.encrypt(xord_block)
+        ciphertext += block
+    return bytes(ciphertext)
+
+
+def aes_cbc_decrypt(data, key, iv):
+    cipher = AES.new(key, AES.MODE_ECB)
+    plaintext = bytearray()
+    xor_block = iv
+    for i in range(0, len(data), 16):
+        ciphertext = data[i: i + 16]
+        decrypted = cipher.decrypt(ciphertext)
+        pt_block = bytes([xor_block[i] ^ decrypted[i] for i in range(16)])
+        xor_block = ciphertext
+        plaintext += pt_block
+    return bytes(unpad(plaintext))
 
 
 # just returns the last block
@@ -40,10 +51,6 @@ def aes_cbc_mac(data, key, iv, no_pad=False):
         ciphertext = cipher.encrypt(xord)
 
     return ciphertext
-
-
-def aes_cbc_decrypt(data, key, iv):
-    return AES.new(key, AES.MODE_CBC, iv).decrypt(data)
 
 
 def ctr(data, key, nonce):
