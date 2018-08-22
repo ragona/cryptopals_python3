836abc3847cbc8f6b17ca6b029118de83acc2640
ragona <ryanragona@gmail.com>
2018-08-22 11:22:52 -0700

    setup for c51

--- c51.py
+++ c51.py
@@ -1,3 +1,51 @@
+import zlib
+import os
+from pals.ciphers import aes_cbc_encrypt
+from salsa20 import Salsa20_xor
+
+
+def oracle(post):
+    return len(encrypt(compress(format_request(post))))
+
+
+def encrypt(data):
+    key = os.urandom(32)
+    iv = os.urandom(8)
+    return Salsa20_xor(data, iv, key)
+
+
+def cbc_encrypt(data):
+    return aes_cbc_encrypt(
+        data=data,
+        key=os.urandom(16),
+        iv=os.urandom(16)
+    )
+
+
+def compress(data):
+    return zlib.compress(data)
+
+
+def format_request(post):
+    return bytes((
+        "POST / HTTP / 1.1\n"
+        "Host: hapless.com\n"
+        "Cookie: sessionid=TmV2ZXIgcmV2ZWFsIHRoZSBXdS1UYW5nIFNlY3JldCE=\n"
+        f"Content-Length: {len(post)}\n"
+        f"{post}"
+    ), "utf-8")
+
+
+def main():
+    a = "sessionid=T"
+    b = "sessionid=S"
+
+    print(oracle(a))
+    print(oracle(b))
+
+
+if __name__ == '__main__':
+    main()
 
 """
 Compression Ratio Side-Channel Attacks
